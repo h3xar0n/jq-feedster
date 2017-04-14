2b29282066d64ff53e95f6b3861bc73e8a14c51a
h3xar0n <aron@glambyadelina.com>
2017-04-14 14:38:57 -0400

    Toggle like button

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var main = function() {
   $('.notification img').click(function(){
     $('.notification-menu').toggle();
   });
+  $('.post .btn').click(function(){
+    $(this).toggleClass('btn-like');
+  });
 };
 
 $(document).ready(main);
